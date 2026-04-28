f81a91826ef57430fffe9fd291bd0238a449d6b7
Alex Nussel <alex.nussel+github@gmail.com>
2026-04-27 19:59:09 -0500

    update to 0.31.2, new repo

--- Formula/pw.rb
+++ Formula/pw.rb
@@ -1,17 +1,17 @@
 class Pw < Formula
   desc "Terminal-based Formula 1 Session Interface"
-  homepage "https://github.com/anussel5559/f1-pitwall"
-  version "0.31.0"
+  homepage "https://github.com/anussel5559/f1pitwall-rs"
+  version "0.31.2"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/anussel5559/f1-pitwall/releases/download/v0.31.0/pw-x86_64-apple-darwin.tar.gz"
-    sha256 "8ed198a243ba8b5b4aa8ff3f74c6db84f603d82bb2b6a706c3406efd79123b9c"
+    url "https://github.com/anussel5559/f1pitwall-rs/releases/download/v0.31.2/pw-x86_64-apple-darwin.tar.gz"
+    sha256 "aac39984e90bb849d270e1ff95190d5ea78e2e3d43ed4016e97a278e4b063129"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/anussel5559/f1-pitwall/releases/download/v0.31.0/pw-aarch64-apple-darwin.tar.gz"
-    sha256 "6d548907596541b2139a2dc2b1d01071d5c822de9580d08009da827eccad13d0"
+    url "https://github.com/anussel5559/f1pitwall-rs/releases/download/v0.31.2/pw-aarch64-apple-darwin.tar.gz"
+    sha256 "2ba45505dc8a1f2f80bdc15e1a37c477b5029732c5f30681c4961bcfd1bc6401"
   elsif OS.linux?
-    url "https://github.com/anussel5559/f1-pitwall/releases/download/v0.31.0/pw-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "55df0b53ae5cbeeada6ae0657d6c879e64d623176c27e0d7dbe1a9fafcadec5e"
+    url "https://github.com/anussel5559/f1pitwall-rs/releases/download/v0.31.2/pw-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "c4c457a5848f9cf318f980c65bc4406c6f8279c541006d2ab18079b86341d9eb"
   end
 
   def install
